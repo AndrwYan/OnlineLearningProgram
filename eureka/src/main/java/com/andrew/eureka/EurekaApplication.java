af5d2f4f5a4fc97bdc43012f452ea0efe0e45414
AndrwYan <beyond1997110@gmail.com>
2021-04-25 13:33:04 +0800

    1.编写了system模块，并注册到了eureka

--- eureka/src/main/java/com/andrew/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/andrew/eureka/EurekaApplication.java
@@ -1,16 +1,20 @@
-package com.andrew;
+package com.andrew.eureka;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
+/*
+*
+* 不要忘记添加注解
+*
+* */
 @SpringBootApplication
 @EnableEurekaServer
-class EurekaApplication {
+public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(EurekaApplication.class,args);
     }
-
 }
