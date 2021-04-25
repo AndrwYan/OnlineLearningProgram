aea48306cb292d66c321fdb4167e2888d73dae03
AndrwYan <beyond1997110@gmail.com>
2021-04-25 10:28:05 +0800

    spring项目初始化

---
+++ src/main/java/com/andrew/OnlinelearnApplication.java
@@ -0,0 +1,13 @@
+package com.andrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinelearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinelearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andrew/OnlinelearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinelearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
