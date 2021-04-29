99341069cade10c7325dca2e822834f2c8c8f1df
AndrwYan <beyond1997110@gmail.com>
2021-04-29 18:33:58 +0800

    1.生成器开发，解决controller模板中的业务名称和模块名称问题

--- generator/src/main/java/com/andrew/generator/ftl/controller.ftl
+++ generator/src/main/java/com/andrew/generator/ftl/controller.ftl
@@ -1,4 +1,4 @@
-package com.andrew.business.controller.admin;
+package com.andrew.${module}.controller.admin;
 
 import com.andrew.server.dto.${Domain}Dto;
 import com.andrew.server.dto.${Domain}PageDto;
@@ -14,9 +14,8 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("/admin/${domain}")
 public class ${Domain}Controller {
-
     private static final Logger LOG = LoggerFactory.getLogger(${Domain}Controller.class);
-    public static final String BUSINESS_NAME = "大章";
+    public static final String BUSINESS_NAME = "${tableNameCn}";
 
     @Resource
     private ${Domain}Service ${domain}Service;
--- generator/src/main/java/com/andrew/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/andrew/generator/server/ServerGenerator.java
@@ -13,15 +13,22 @@ import java.util.Map;
 
 public class ServerGenerator {
 
+    static String MODULE="business";
     static String toServicePath = "server\\src\\main\\java\\com\\andrew\\server\\service\\";
-    static String toControllerPath = "business\\src\\main\\java\\com\\andrew\\business\\controller\\admin\\";
+    static String toControllerPath = MODULE+"\\src\\main\\java\\com\\andrew\\"+MODULE+"\\controller\\admin\\";
+
     public static void main(String[] args) throws IOException, TemplateException {
         //
         String Domain="Section";
         String domain="section";
+        String tableNameCn="小节";
+        String module=MODULE;
+
         Map<String, Object> map = new HashMap<>();
         map.put("Domain",Domain);
         map.put("domain",domain);
+        map.put("tableNameCn",tableNameCn);
+        map.put("module",module);
 
         FreeMarkerUtil.initConfig("service.ftl");
         FreeMarkerUtil.
