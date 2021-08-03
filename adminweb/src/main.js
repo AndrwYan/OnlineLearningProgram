e48c1e6c165bf172d930320923e5756a59fb65d1
name222 <liup1195930531@163.com>
2021-08-03 19:52:26 +0800

    增加ace admin模板

--- adminweb/src/main.js
+++ adminweb/src/main.js
@@ -1,12 +1,12 @@
 
-
 import Vue from 'vue'
-import App from './app.vue'
+import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip=false
 
 new Vue({
         router,
+        //渲染到app，
         render: h => h(App),
     }).$mount('#app')
