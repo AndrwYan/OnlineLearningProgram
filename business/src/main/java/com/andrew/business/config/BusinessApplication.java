c437df6a8f15903b70f1b957fd17b799a5a743b2
name222 <liup1195930531@163.com>
2021-08-07 18:19:47 +0800

    1.怎加business模块，并解决bug

--- business/src/main/java/com/andrew/business/config/BusinessApplication.java
+++ business/src/main/java/com/andrew/business/config/BusinessApplication.java
@@ -1,17 +1,27 @@
 package com.andrew.business.config;
-
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+
 @EnableEurekaClient
 @ComponentScan("com.andrew")
 @MapperScan("com.andrew.server.mapper")
 public class BusinessApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(BusinessApplication.class,args);
+        SpringApplication app = new SpringApplication(BusinessApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("Business地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 }
