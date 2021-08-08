0260788f9ce66f2111792e90bf270da8d7d2aadf
name222 <liup1195930531@163.com>
2021-08-08 10:25:56 +0800

    配偶gateway配置类，将之前的模块的配置类放在gateway模块下。

--- gateway/src/main/java/com/andrew/gateway/GateWayApplication.java
+++ gateway/src/main/java/com/andrew/gateway/GateWayApplication.java
@@ -3,11 +3,31 @@ package com.andrew.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class GateWayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GateWayApplication.class,args);
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(Boolean.TRUE);
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/",config);
+        return new CorsWebFilter(source);
     }
 }
